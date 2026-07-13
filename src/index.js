288b59ee3d252b666ef170b0691393623ebb3d2d
Geraldine88 <geraldinennene@gmail.com>
2026-07-13 10:26:00 -0700

    Adding Google Analytics via env var

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Google Analytics
+const gaId = process.env.REACT_APP_GA_ID;
+if (gaId) {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ window.dataLayer.push(arguments); }
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', gaId);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
